1bcefb26c9fb8943b3580b895bb87770b148fd06
Muhammad Bilal Raza <bilalraza9170@gmail.com>
2025-03-20 15:06:42 +0500

    second commit

--- index.php
+++ index.php
@@ -4,3 +4,5 @@ require "vendor/autoload.php";
 
 //Loading the data
 $data = new \Phpml\Dataset\CsvDataset("./data/insurance.csv",1,true);
+//Preprocessing the data
+$dataset = new \Phpml\CrossValidation\RandomSplit($data,0.2,156);
