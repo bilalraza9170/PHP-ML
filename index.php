311ed2ba502a49d60c7a34f33de530ddcc7c5239
Muhammad Bilal Raza <bilalraza9170@gmail.com>
2025-03-20 16:20:49 +0500

    third commit

--- index.php
+++ index.php
@@ -6,3 +6,17 @@ require "vendor/autoload.php";
 $data = new \Phpml\Dataset\CsvDataset("./data/insurance.csv",1,true);
 //Preprocessing the data
 $dataset = new \Phpml\CrossValidation\RandomSplit($data,0.2,156);
+//$dataset->getTestSamples();
+//$dataset->getTestLabels();
+//$dataset->getTrainSamples();
+//$dataset->getTrainLabels();;
+//Training
+$regression = new \Phpml\Regression\LeastSquares();  //Linear Regression
+$regression->train($dataset->getTrainSamples(),$dataset->getTrainLabels());
+
+$predict = $regression->predict($dataset->getTestSamples()); //To check
+//Evaluate
+$score = \Phpml\Metric\Regression::r2Score($dataset->getTestLabels(),$predict); //static method no instance
+//echo "r2Score is ".$score."\n";
+//Making prediction with trained models
+var_dump($regression->predict([80]));
