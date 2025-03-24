605a7c8524728e9a664966a56f1f8340f3af35f4
Muhammad Bilal Raza <bilalraza9170@gmail.com>
2025-03-24 15:53:33 +0500

    KNearestNeighbour

--- index.php
+++ index.php
@@ -15,17 +15,20 @@ $dataset = new \Phpml\CrossValidation\StratifiedRandomSplit($data,0.2,156);
 /*Training*/
 //$regression = new \Phpml\Regression\LeastSquares();  //Linear Regression
 //Support Vector Regressor: Find the best line 0r plan that separate different groups and match the nearest match
-$regression = new \Phpml\Regression\SVR();
-$regression->train($dataset->getTrainSamples(),$dataset->getTrainLabels());
+//$regression = new \Phpml\Regression\SVR();
+//Classification there is no flow of data only discrete data. opp of regression.
+//KNN: KnearestNeighbours assign label to u based on the closed neighbour
+$classification = new \Phpml\Classification\KNearestNeighbors();
+$classification->train($dataset->getTrainSamples(),$dataset->getTrainLabels());
 
-$predict = $regression->predict($dataset->getTestSamples()); //To check
+$predict = $classification->predict($dataset->getTestSamples()); //To check
 /*Evaluate*/
-$score = \Phpml\Metric\Regression::r2Score($dataset->getTestLabels(),$predict); //static method no instance
-echo "r2Score is ".$score."\n";
-
-foreach ($predict as &$target){ //& only change the positioned value
-    $target = round($target,0);
-}
+//$score = \Phpml\Metric\Regression::r2Score($dataset->getTestLabels(),$predict); //static method no instance
+//echo "r2Score is ".$score."\n";
+//
+//foreach ($predict as &$target){ //& only change the positioned value
+//    $target = round($target,0);
+//}
 $accuracy = \Phpml\Metric\Accuracy::score($dataset->getTestLabels(),$predict); //mainly for classification so no decimal
 echo "accuracy is ".$accuracy."\n";
 
